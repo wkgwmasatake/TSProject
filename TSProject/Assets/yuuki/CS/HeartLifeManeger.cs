daec05f40bcc35f2eb9898fe57fa1eeb032ce82c
LAPTOP-UEIRJFST\moyo0 <yuki-t-17d@stu.kbc.ac.jp>
2019-01-18 12:06:44 +0900

    元ファイル追加

---
+++ TSProject/Assets/yuuki/CS/HeartLifeManeger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartLifeManeger : MonoBehaviour
+{
+    public GameObject GOHeart;
+
+    const int PlayerLifeMax = 20;
+    int PlayerLife;
+
+    int XMax = PlayerLifeMax / 2;
+
+    float XPos;
+    float YPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerLife = PlayerLifeMax;
+        HeartCreate(PlayerLife);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlayerLife -= 1;
+            HeartCreate(PlayerLife);
+        }
+    }
+
+    void HeartCreate(int PlayerLife)
+    {
+        HeartDestroy();
+
+        int setLife = PlayerLife;
+
+        for (YPos = 0; 2 > YPos; YPos++)
+        {
+            for (XPos = 0; setLife > XPos && 10 > XPos; XPos++)
+            {
+                Instantiate(GOHeart, new Vector2(XPos, -YPos), transform.rotation);
+            }
+            setLife -= 10;
+        }
+    }
+    
+    void HeartDestroy()
+    {
+         GameObject[] Hearts = GameObject.FindGameObjectsWithTag("Heart");
+
+        foreach (GameObject Heart in Hearts)
+        {
+                Destroy(Heart);
+        }
+    }
+}
---
+++ TSProject/Assets/yuuki/CS/PowerGauge.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerGauge : MonoBehaviour {
+
+    const int PowerMax = 100;   //ゲージの最大量
+    const float PlusPower = PowerMax*0.01f;    //パワーを足す量
+    float Power;    //パワー保持
+
+    int Count;      //Count MAXになったら反転
+
+	// Use this for initialization
+	void Start () {
+
+        Power = 0;
+        GaugeRender(0);
+
+        Count = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        OnClick();
+	}
+
+    public void GaugeRender(float Power)
+    {
+        this.GetComponent<Image>().fillAmount = (Power / PowerMax);
+    }
+
+    private void OnClick()
+    {
+        if(Input.GetMouseButton(0)){
+            Count++;
+
+            if(Count / 100 == 0)
+                GaugeRender(Power += PlusPower);
+            if (Count / 100 == 1)
+                GaugeRender(Power -= PlusPower);
+            if (Count > 200)
+                Count = 0;
+        }
+    }
+}
