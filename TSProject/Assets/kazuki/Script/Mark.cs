9424408f7b9ecd9d85a003720b0b4cc7630e8b93
kazuki24 <kazu2324ki@gmail.com>
2019-01-28 08:33:18 +0900

    source変えました

--- TSProject/Assets/kazuki/Script/Mark.cs
+++ TSProject/Assets/kazuki/Script/Mark.cs
@@ -7,32 +7,60 @@ public class Mark : MonoBehaviour {
     public GameObject Boll;
     public GameObject boll;
     private Vector3 vec;
+    float Speed = 1.0f;
     bool flg5 = true;
+
+    float Vx2 ;
+    float Vy2 ;
+    float Vz2 ;
+    float Powe = 1.0f;
+
     // Use this for initialization
     void Start () {
          boll = GameObject.Find("Sphere");
         vec = boll.GetComponent<TestScript>().Vec;
+        Vx2 = boll.GetComponent<TestScript>().Vx;
+        Vy2 = boll.GetComponent<TestScript>().Vy;
+        Vz2 = boll.GetComponent<TestScript>().Vz;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.anyKey)
+        if (Input.anyKey && !(Input.GetKey(KeyCode.K)))
         {
             flg5 = true;
-
+            vec = boll.GetComponent<TestScript>().Vec;
+            
+            Vx2 = boll.GetComponent<TestScript>().Vx;
+            Vy2 = boll.GetComponent<TestScript>().Vy;
+            Vz2 = boll.GetComponent<TestScript>().Vz;
         }
+        //if (Input.GetKey(KeyCode.P))
+        //{
+        //    Powe += 0.01f;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Powe -= 0.01f;
+        //}
         if (flg5 == true)
         {
             
-            for (int i = 1; i < 20; i++)
+            for (int i = 1; i < 50; i++)
             {
-                Debug.Log("OK");
+                //Debug.Log("OK");
                 
                 GameObject item;
+
+                Vy2 -= 0.05f;
+
+                vec += new Vector3(Vx2, Vy2, Vz2);
                 
-                vec += new Vector3(0, -0.05f, 0); 
-                Vector3 ivec = boll.transform.position + vec*i*5;
+
+
+
+                Vector3 ivec = boll.transform.position + vec*5f;
                 item = Instantiate(Boll, ivec,Quaternion.identity);
             }
             flg5 = false;
